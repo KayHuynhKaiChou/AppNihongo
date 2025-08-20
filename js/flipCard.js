d67a90e8a759a775a4f98a7f7d583d7d9bed34b5
KayHuynhKaiChou <huynhgiaquy123@gmail.com>
2025-08-20 11:44:54 +0700

    add toeic volcabulary

--- js/flipCard.js
+++ js/flipCard.js
@@ -1,6 +1,14 @@
-function flipCard(card) {
+function flipCard(card, prevCard) {
     var front = card.querySelector('.front');
     var back = card.querySelector('.back');
+    
+    if(prevCard && card !== prevCard) {
+        const frontPrevCard = prevCard.querySelector('.front');
+        const backPrevCard = prevCard.querySelector('.back');
+
+        frontPrevCard.style.display = 'block';
+        backPrevCard.style.display = 'none';
+    } 
 
     if (front.style.display === 'none') {
         front.style.display = 'block';
--- js/handleShowCards.js
+++ js/handleShowCards.js
@@ -1,22 +1,48 @@
 
 import listHiragana from './hiragana.js'
 import listKanji from './kanji.js'
+import toeic from './toeic.js';
 
-var listTuVung = (location.href+'').includes('hiragana.html') ? listHiragana : listKanji
+let listTuVung = []
+let typeVolcabulary = [...location.href.split('/')].pop();
+
+switch (typeVolcabulary) {
+    case 'hiragana.html':
+        listTuVung = listHiragana;
+        break;
+    case 'kanji.html':
+        listTuVung = listKanji;
+        break;
+    case 'toeic.html':
+        listTuVung = toeic;
+        break;
+    default:
+        break;
+}
 
 var flashContai = document.getElementById('flashcards-container');
 
 var listPreviousIndex = [];
 
+var prevCardSelect = null;
+
 flashContai.innerHTML = listTuVung.map((index) => {
     var validIndex = conductRandom();
     return `
-    <div class="col-md-3 flashcard" onclick="flipCard(this)">
-        <div class="front">${listTuVung[validIndex].tv}</div>
-        <div class="back">${listTuVung[validIndex].mean}</div>
-    </div>
+        <div class="col-md-3 flashcard">
+            <div class="front">${listTuVung[validIndex].tv}</div>
+            <div class="back">${listTuVung[validIndex].mean}</div>
+        </div>
     `
 }).join('');
+
+// Gán sự kiện
+document.querySelectorAll('.flashcard').forEach(card => {
+    card.addEventListener('click', function () {
+        flipCard(card, prevCardSelect);
+        prevCardSelect = card; // cập nhật sau khi lật
+    });
+});
 
 function conductRandom(){
     do {
